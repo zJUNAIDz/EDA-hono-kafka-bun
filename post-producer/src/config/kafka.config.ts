e75ae7529d949b58e26e68fd8fb8f6e13b68f83d
Junaid Shaikh <junaidshaikh3967@gmail.com>
2024-10-05 21:15:11 +0530

    add necessary methods to kafka class

--- post-producer/src/config/kafka.config.ts
+++ post-producer/src/config/kafka.config.ts
@@ -17,5 +17,52 @@ class KafkaConfig {
     this.admin = this.kafka.admin();
   }
 
-  
+  async connect() {
+    try {
+      console.log("Connecting to Producer");
+      await this.producer.connect();
+      console.log("Connecting to Admin");
+      await this.admin.connect();
+      console.log("Connected")
+    } catch (err) {
+      console.error("Failed to Connect: \n", err);
+    }
+  }
+
+
+  async createTopic(topic: string) {
+    try {
+      console.log("Creating topic: ", topic);
+      await this.admin.createTopics({ topics: [{ topic }] });
+      console.log("Topic created.")
+    } catch (err) {
+      console.log("Failed to create topic:", err);
+    }
+  }
+
+  async sendToTopic(topic: string, message: string) {
+    try {
+      console.log("Sending message: ", message);
+      await this.producer.send({
+        topic,
+        messages: [{ value: message }]
+      });
+      console.log("Message sent.")
+    } catch (err) {
+      console.log("Send to Topic failed: \n", err);
+    }
+  }
+
+  async disconnect() {
+    try {
+      console.log("Disconnecting kafka...")
+      await this.admin.disconnect();
+      await this.producer.disconnect();
+      console.log("Disconnected.")
+    } catch (err) {
+      console.log("Failed to disconnect: \n", err);
+    }
+  }
 }
+
+export default new KafkaConfig();
